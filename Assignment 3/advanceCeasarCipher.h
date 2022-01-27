ca7376d47b922b47f5ffee4d45bebea5dde2da6d
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-01-27 09:55:12 -0600

    Assignment 3 Updated

--- Assignment 3/advanceCeasarCipher.h
+++ Assignment 3/advanceCeasarCipher.h
@@ -3,7 +3,22 @@
 #endif
 #include "caesarCipher.h"
 
-class AdvanceCeasarCipher{
+class AdCipher : public caesarCipher{
+    string arr;
+    public:
+        AdCipher() : caesarCipher(1){}
+        AdCipher(int x): caesarCipher(x){}
 
+
+        void getMessage(){
+            cout<<"Enter your Message";
+            cin>>arr;
+            arr = encrypt(arr);
+        }
+        void print(){
+            caesarCipher :: print();
+            cout<<endl<<arr<<endl;
+            cout<<decrypt(arr)<<endl;
+        }
 
 };
--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "advanceCeasarCipher.h"
 
 using namespace std;
 
 int main(){
-
+    AdCipher obj(5);
+    obj.getMessage();
+    obj.print();
     return 0;
 }
