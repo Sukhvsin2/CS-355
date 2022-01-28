ade2936c8bf10c902c4fe1742d433b56c9fa3786
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-01-28 09:47:13 -0600

    Update advanceCeasarCipher.h

--- Assignment 3/advanceCeasarCipher.h
+++ Assignment 3/advanceCeasarCipher.h
@@ -5,20 +5,41 @@
 
 class AdCipher : public caesarCipher{
     string arr;
+    char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     public:
-        AdCipher() : caesarCipher(1){}
-        AdCipher(int x): caesarCipher(x){}
+        void shiftedAlpha(int shift){
+            int shiftValue = shift;
+            for(int i=0;i<26;i++){
+	    		if (alpha[i] >= 97 && alpha[i] <= 122){
+	   			    if (alpha[i]+shiftValue > 122){ 
+			    		alpha[i]=122-(alpha[i]%97);	
+		    		}
+				    else{
+					    alpha[i]+=shiftValue;
+				    }		
+			    }
+		    }
+        }
+        AdCipher() : caesarCipher(1){
+        }
+        AdCipher(int x): caesarCipher(x){
+        }
 
 
         void getMessage(){
             cout<<"Enter your Message";
             cin>>arr;
             arr = encrypt(arr);
+            shiftedAlpha(limit);
         }
         void print(){
             caesarCipher :: print();
             cout<<endl<<arr<<endl;
             cout<<decrypt(arr)<<endl;
+            cout<<"\nShifted Values : ";
+            for(int i = 0; i < 26; i++){
+                cout << alpha[i];
+            }
         }
 
 };
