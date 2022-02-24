1e82f683eb6aa0fab321613180874055b4f10692
Sukhvinder Singh <sukhvindersingh@sukhvinders-air.main.local.una.edu>
2022-02-24 14:59:16 -0600

    Cases Working

--- Assignment 5/Assignment 5/ArrayList.h
+++ Assignment 5/Assignment 5/ArrayList.h
@@ -8,7 +8,7 @@ using namespace std;
 class ArrayList{
     int *arr, size;
     public:
-    ArrayList(int size=5){
+    ArrayList(int size=5000){
         this->size = size;
         arr = new int[size];
         initlizeArray(arr, size);
@@ -19,7 +19,7 @@ class ArrayList{
         sleep(1);
         // end
         for(int i=0;i<size;i++){
-            arr[i] = rand() % 5 + 1;
+            arr[i] = rand() % size + 1;
         }
     }
     ArrayList operator=(ArrayList &obj){
@@ -27,7 +27,6 @@ class ArrayList{
         return *this;
     }
     void copy(ArrayList &obj){
-        cout<<"Copy function\n";
         for(int i=0;i<5;i++){
             arr[i] = obj.arr[i];
         }
@@ -38,27 +37,44 @@ class ArrayList{
         *b = temp;
     }
     void bubbleSort() { 
+        int counter = 0;
         for (int i = 0; i < size-1; i++)
             for (int j = 0; j < size-i-1; j++) 
-                if (arr[j] > arr[j+1]) 
+                if (arr[j] > arr[j+1]){
+                    counter++;
                     swap(&arr[j], &arr[j+1]); 
+                }
+
+        cout<<"Number of Comparison in Bubble Sort: "<<counter<<endl;
     } 
     void selectionSort(){
         int maxIndex = 0;
+        int counter = 0;
         for(int i=size-1;i>=0;i--){
             maxIndex = 0;
             for(int j=0; j<=i;j++){
-                if(arr[j] > arr[maxIndex] ) maxIndex= j;
+                if(arr[j] > arr[maxIndex] ) {
+                    counter++;
+                    maxIndex= j;
+                }
             }
-            if(maxIndex != i) swap(&arr[i], &arr[maxIndex]);
+            if(maxIndex != i) {
+                swap(&arr[i], &arr[maxIndex]);
+                counter++;
+            }
         }
+        cout<<"Number of Comparison in Selection Sort: "<<counter<<endl;
     }
     void insertionSort(){
+        int counter = 0;
         for(int i=0;i<size-1;i++){
             for(int j=i; j>=0; j--){
-                if(arr[j] < arr[j-1]) swap(&arr[j], &arr[j-1]);
+                if(arr[j] < arr[j-1]){
+                    counter++;
+                    swap(&arr[j], &arr[j-1]);}
             }
         }
+        cout<<"Number of Comparison in Insertion Sort: "<<counter<<endl;
     }
     void print(){
         for(int i=0;i<5;i++){
--- Assignment 5/Assignment 5/main.cpp
+++ Assignment 5/Assignment 5/main.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 int main()
 {
-    ArrayList list1, list2;
-    cout<<"Before: ";
-    list1.print();
-    list1.insertionSort();
-    cout<<"After: ";
-    list1.print();
+    ArrayList list1, list2, list3;
+    list2 = list1;
+    list3 = list2;
+
+    // list1.print();
+    list1.bubbleSort();
+    list2.selectionSort();
+    list3.insertionSort();
+
+
+
     return 0;
 }
