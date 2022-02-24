380d45d2330c137a4e2f654530215e91254f6682
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2022-02-24 13:34:51 -0600

    Sorting functions added

--- Assignment 5/Assignment 5/ArrayList.h
+++ Assignment 5/Assignment 5/ArrayList.h
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<cstdlib>
+#include<time.h>
 #include <unistd.h>
 
 using namespace std;
 
 class ArrayList{
-    int *arr;
+    int *arr, size;
     public:
     ArrayList(int size=5){
         arr = new int[size];
@@ -31,6 +32,32 @@ class ArrayList{
         cout<<"Copy function\n";
         for(int i=0;i<5;i++){
             arr[i] = obj.arr[i];
+        }
+    }
+    void bubbleSort(int *arr, size){
+        for(int i=0;i<size-1;i++){
+            for(int j=0;j<size-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr[i],arr[j]);
+                }
+            }
+        }
+    }
+    void selectionSort(int *arr, size){
+        int maxIndex = 0;
+        for(int i=size-1;i>=0;i--){
+            maxIndex = 0;
+            for(int j=0; j<=i;j++){
+                if(arr[j] > arr[maxIndex] ) maxIndex= j;
+            }
+        }
+        if(maxIndex != i) swap(arr[i], arr[maxIndex]);
+    }
+    void insertionSort(int *arr, size){
+        for(int i=0;i<size-1;i+){
+            for(int){
+
+            }
         }
     }
     void print(){
